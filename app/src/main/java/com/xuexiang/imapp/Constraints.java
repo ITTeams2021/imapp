25ecb516e56c06ea61f106c7f1b4551e81f09e0d
suyuan32 <1137661202@qq.com>
2021-04-28 13:37:21 +0800

    修改了constraints

---
+++ app/src/main/java/com/xuexiang/imapp/Constraints.java
@@ -0,0 +1,18 @@
+package com.xuexiang.imapp;
+
+import org.json.JSONArray;
+
+/**
+ * @author: Su Yuan
+ * @Date: 2021/4/14
+ * @Decription:
+ */
+public class Constraints {
+
+    public static JSONArray contact_list;
+    public static String current_chat_user;
+
+    public static final String TCP_IP_ADDRESS = "159.75.220.96";
+    public static final Integer TCP_PORT = 8888;
+
+}
